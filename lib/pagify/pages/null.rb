778902b5b1f1e51ba72b7b675c0a7f003a46c283
godfat <godfat@godfat.org>
2008-12-09 18:09:05 +0800

    Revert "null page has empty data." seems latest dm-core causes problems.
    This reverts commit 80fc3bb78a428190e68f4a8b929ddd0a6edc2ce6.

--- lib/pagify/pages/null.rb
+++ lib/pagify/pages/null.rb
@@ -4,10 +4,7 @@ module Pagify
   # a null page that stubs anything to 0 or [], etc.
   class NullPage < BasicPage
     include Singleton
-    def initialize
-      super(NullPager.instance, 0)
-      @data = []
-    end
+    def initialize; super(NullPager.instance, 0); end
   end
 
 end # of Pagify
