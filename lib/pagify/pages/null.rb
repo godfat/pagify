80fc3bb78a428190e68f4a8b929ddd0a6edc2ce6
godfat <godfat@godfat.org>
2008-12-09 17:59:58 +0800

    null page has empty data.

--- lib/pagify/pages/null.rb
+++ lib/pagify/pages/null.rb
@@ -4,7 +4,10 @@ module Pagify
   # a null page that stubs anything to 0 or [], etc.
   class NullPage < BasicPage
     include Singleton
-    def initialize; super(NullPager.instance, 0); end
+    def initialize
+      super(NullPager.instance, 0)
+      @data = []
+    end
   end
 
 end # of Pagify
