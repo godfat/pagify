acd544a5640c0f15f93344ccefae9631acb4c3f7
godfat <godfat@godfat.org>
2008-12-11 12:21:50 +0800

    don't autoload VERSION.

--- lib/pagify.rb
+++ lib/pagify.rb
@@ -3,8 +3,6 @@ module Pagify
   autoload         :ArrayPager, 'pagify/array'
   autoload    :DataMapperPager, 'pagify/data_mapper'
   autoload  :ActiveRecordPager, 'pagify/active_record'
-
-  autoload :VERSION, 'pagify/version'
 end
 
 require 'pagify/basic'
