944987ced77f2e806fcaf9dd023a66e18fe1e6a1
Lin Jen-Shin <godfat@godfat.org>
2009-07-29 02:06:53 +0800

    [html.rb] don't add a newline in links_full

--- lib/pagify/helpers/html.rb
+++ lib/pagify/helpers/html.rb
@@ -22,7 +22,7 @@ module Pagify
 
       def links_full page, &block
         page = normalize_page(page)
-        "#{links_navigate(page, &block)}<br />\n#{links(page, &block)}"
+        "#{links_navigate(page, &block)}<br />#{links(page, &block)}"
       end
 
       def links_navigate page
