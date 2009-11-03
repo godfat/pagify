01c76c7120b7fd057fc9f332795b148e79274a24
Lin Jen-Shin <godfat@godfat.org>
2009-11-03 13:58:33 +0800

    [data_mapper.rb] fixed tests for dm-core 0.10.1, check query:
      check query to prevent strange errors...

--- lib/pagify/pager/data_mapper.rb
+++ lib/pagify/pager/data_mapper.rb
@@ -13,13 +13,23 @@ module Pagify
 
       super(opts.merge(
         :fetcher => lambda{ |offset, per_page|
-          model.send :with_scope, query do
+          # TODO: i am not sure why do we need this check?
+          if query == model.query
             model.all(query_opts.merge(:offset => offset, :limit => per_page))
+          else
+            model.send(:with_scope, query) do
+              model.all(query_opts.merge(:offset => offset, :limit => per_page))
+            end
           end
         },
         :counter => lambda{
-          model.send :with_scope, query do
+          # TODO: i am not sure why do we need this check?
+          if query == model.query
             model.count(query_opts)
+          else
+            model.send(:with_scope, query) do
+              model.count(query_opts)
+            end
           end
         }))
     end
