2707b00645d22302d0935ce6480c9165d1202ddb
Lin Jen-Shin <godfat@godfat.org>
2011-11-12 00:32:06 +0800

    active_record.rb: adopt rails 3 changes

--- lib/pagify/pagifier/active_record.rb
+++ lib/pagify/pagifier/active_record.rb
@@ -1,7 +1,13 @@
 
 require 'pagify/pagifier/abstract'
 
-[ActiveRecord::Base, ActiveRecord::Associations::AssociationCollection].each{ |klass|
+proxy = if defined?(ActiveRecord::Associations::CollectionProxy)
+          ActiveRecord::Associations::CollectionProxy
+        else
+          ActiveRecord::Associations::AssociationCollection
+        end
+
+[ActiveRecord::Base, proxy].each{ |klass|
   klass.module_eval do
     extend Pagify::Pagifier
     def self.pagify_pager_create model, opts
